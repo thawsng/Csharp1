a0e62b43a2ded19f631b701d963a89433e600c84
DoanTrongThang <email_cua_ban>
2021-06-18 15:12:43 +0700

    baikiemtra

---
+++ DOANTRONGTHANG-T2009A/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace DOANTRONGTHANG_T2009A
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        
+            public int Age { get; set; }
+        
+        }
+    }
+    
