dfc7e6120c7ebdb4b0622a0e3af1658d2c607bec
DoanTrongThang <email_cua_ban>
2021-06-18 15:22:13 +0700

    Homework by NDT

---
+++ Exam2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exam2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SearchWord();
+        }
+
+        private static void SearchWord()
+        {
+            string str;
+            string word;
+            int count = 0;
+            Console.WriteLine("Enter a String");
+            str = Console.ReadLine();
+            Console.WriteLine("Enter a word to search");
+            word = Console.ReadLine();
+        }
+    }
+}
